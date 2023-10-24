8b65102de480e8d0a4fa0d37df47bf218ea2269f
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-24 16:47:02 +0900

    createアクションの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
     @posts = Post.all
   end
@@ -8,13 +9,17 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to '/'
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   def post_params
-    params.require(:post).permit(:image, :title, :explanation, :genre_id, :shelf_number_id).merge(user_id)
+    params.require(:post).permit(:image, :title, :explanation, :genre_id, :shelf_number_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ApplicationRecord
-  beklongs_to :user
+  belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :explanation, presence: true
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shelf_number_id, numericality: { other_than: 1, message: "can't be blank" }
 
 end
