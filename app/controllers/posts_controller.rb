ca3e68b3dee31bff8800f2af41b3e27d23c1eb14
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-26 18:35:56 +0900

    詳細・編集・一覧表示・削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
 
   def new
@@ -14,6 +14,29 @@ class PostsController < ApplicationController
       redirect_to '/'
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    if@post = Post.find(params[:id])
+      @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
