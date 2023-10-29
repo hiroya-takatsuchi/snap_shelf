dac35920b8f479dc6c61b4ef10675222b4ee5d18
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-29 16:47:37 +0900

    バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,12 @@ class Post < ApplicationRecord
   has_many :likes
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shelf_number_id, numericality: { other_than: 1, message: "can't be blank" }
+
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end
@@ -24,10 +30,4 @@ class Post < ApplicationRecord
   #   end
   # end
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :image, presence: true
-  validates :title, presence: true
-  validates :explanation, presence: true
-  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shelf_number_id, numericality: { other_than: 1, message: "can't be blank" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :nickname, presence: true
+  validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+
   def total_likes
     likes.count
   end
