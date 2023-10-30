c05116bbfd7397616f1c57de4b0a0443dbb9ae7e
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-30 20:51:05 +0900

    検索フォーム、部分テンプレート

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,8 +1,7 @@
 class Area < ActiveHash::Base
   self.data = [
-    { id: 1, name: 'エリアを検索' },
-    { id: 2, name: '都市型' },
-    { id: 3, name: '郊外型' }
+    { id: 1, name: '都市型' },
+    { id: 2, name: '郊外型' }
   ]
 
   include ActiveHash::Associations
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :nickname, presence: true
-  validates :prefectures_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefectures_id, presence: true
+  validates :area_id, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
   def total_likes
